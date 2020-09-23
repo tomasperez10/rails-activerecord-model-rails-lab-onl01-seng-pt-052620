763d36b3f633ce5fc3f2a1f6ef89b1aac5ca744d
Tomas Perez <tomasitopg6@gmail.com>
2020-09-23 16:26:49 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,6 +3,9 @@ class CreateStudents < ActiveRecord::migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
+
+      t.timestamps null: false
     end
-    
+    end
+
 end
